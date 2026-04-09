2e18a0bee4bed437018b2cb17fff76e200e1b630
Scafu <alessandro.mazzariol04@gmail.com>
2026-04-09 19:02:16 +0200

    fix: removed MOCK_NATS env

--- src/nats/nats.module.ts
+++ src/nats/nats.module.ts
@@ -1,16 +1,12 @@
 import { Module } from '@nestjs/common';
 import { JetStreamClient } from './jetstream.client';
-import { MockJetStreamClient } from './mock-jetstream.client';
 import { NatsJetStreamClient } from './nats-jetstream.client';
 
 @Module({
   providers: [
     {
       provide: JetStreamClient,
-      useClass:
-        process.env.MOCK_NATS === 'false'
-          ? NatsJetStreamClient
-          : MockJetStreamClient,
+      useClass: NatsJetStreamClient,
     },
   ],
   exports: [JetStreamClient],
